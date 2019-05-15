1018636fd1d097775c95a625d8b92683be60462f
matjukov-nikolaj <blatnoi.kolyan@yandex.ru>
2019-05-15 09:12:09 +0300

    added solution for task2

--- PP2/BankClient.cpp
+++ PP2/BankClient.cpp
@@ -9,18 +9,16 @@ CBankClient::CBankClient(CBank *bank, unsigned id)
 	CreateThread(NULL, 0, ThreadFunction, this, 0, NULL);
 }
 
-
 unsigned CBankClient::GetId()
 {
 	return m_id;
 }
 
-
 DWORD WINAPI CBankClient::ThreadFunction(LPVOID lpParam)
 {
 	CBankClient *client = (CBankClient*)lpParam;
 	// TODO: srand(client->m_id);
-	// srand(client->m_id);
+	srand(client->m_id);
 	while (true)
 	{
 		Sleep(GetSleepDuration(client));
@@ -35,7 +33,8 @@ unsigned CBankClient::GetSleepDuration(CBankClient *client)
 	// TODO: check correctness of running application with no sleep, even in CBank
 
 	// 1000 .. 3999
-	return (1000 + rand() % 3000) * (client->m_id + 1);
+	// Максмальный остаток от деления на 2000 это 2000. Поэтому от 1 до 3 секунд остается
+	return (1000 + rand() % 2000);
 }
 
 
